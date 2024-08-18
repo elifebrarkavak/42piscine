a37c59b7794b8bfa16a7a3d7de7808189de4f84d
Elif Ebrar Kavak <132308110+elifebrarkavak@users.noreply.github.com>
2024-08-18 11:06:20 +0300

    Add files via upload

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elikavak <elikavak@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/14 15:06:33 by elikavak          #+#    #+#             */
+/*   Updated: 2024/08/14 16:28:41 by elikavak         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int		a;
+	int		b;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar(a / 10 + '0');
+			ft_putchar(a % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			if (a != 98 || b != 99)
+			{
+				write(1, ", ", 2);
+			}
+			b++;
+		}
+		a++;
+	}
+}
---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elikavak <elikavak@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/14 14:17:38 by elikavak          #+#    #+#             */
+/*   Updated: 2024/08/14 16:29:04 by elikavak         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else if (nb < 0 && nb != -2147483648)
+	{
+		ft_putchar(45);
+		nb = nb * -1;
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
